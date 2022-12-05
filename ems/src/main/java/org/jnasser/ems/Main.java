faf3635aaae84fa99ea2e8636030500b47c6f2ce
yusufnasserdev <yusufnassereng@gmail.com>
2022-12-05 22:27:39 +0200

    Added IntelliJ project

---
+++ ems/src/main/java/org/jnasser/ems/Main.java
@@ -0,0 +1,7 @@
+package org.jnasser.ems;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ ems/src/main/java/org/jnasser/ems/gui/MainWindow.java
@@ -0,0 +1,9 @@
+package org.jnasser.ems.gui;
+
+/* REFERENCES:
+ * https://medium.com/@keeptoo/javafx-java-modern-ui-design-starter-pack-aab1c331fd3c
+ * https://github.com/JabRef/jabref
+ */
+
+public class MainWindow {
+}
